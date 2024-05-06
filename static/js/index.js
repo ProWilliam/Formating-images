bd51bb235d4be1758e66262e3318fbc725550d9c
William Torre <pro.williamtoro@gmail.com>
2024-05-06 12:52:03 -0500

    Aliustener is added for the button when it is clicked, the following functons are created: selectImage to capture the image, sendData to send the data in formData format, transformImageToBinay to create the bynary because the conversion is asynchronouse, animationLoadingImage to show progressive loading, progressbarUpDate so that when fully loaded it appears or not in the view

--- static/js/index.js
+++ static/js/index.js
@@ -49,3 +49,96 @@ function insertResolution(resolution) {
     console.error("There are not enough child elements: ", childrenElements.length);
   }
 } 
+
+var button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+
+  if(document.querySelector('#drop-area img')){
+    animationLoadingImage();
+    selectImage();
+  }else {
+    var alert = document.getElementById('alertDanger')
+    alert.style.display = 'block';
+  }
+  
+});
+
+
+function selectImage() {
+  const imgElement = document.querySelector('#drop-area img');
+  const srcImage = imgElement.src;
+
+  if (srcImage) {
+    sendData(srcImage);
+  } else {
+    console.error("No image available.");
+  }
+}
+
+async function sendData(file_image) {
+  
+  try {
+    
+    const fileImage = await transformImageToBinary(file_image);
+
+    const formData = new FormData();
+
+    formData.append('image', fileImage);
+
+    const response = await fetch('/formatting_image', {
+      method: 'POST',
+      body: formData
+    });
+
+    if (response.ok) {
+      console.log('Sucessful sended file image:', {response});
+      
+    } else {
+      console.error('Error sended file image:', response.statusText);
+    }
+    
+  } catch (error) {
+    console.error('Error of send Image:', error);
+  }
+}
+
+async function transformImageToBinary(file_image){
+  
+  const blob = await fetch(file_image)
+    .then(response => response.blob());
+
+  const fileImage = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+
+  return fileImage;
+}
+
+function animationLoadingImage() {
+  var progressBar = document.getElementById('progress-bar');
+  var dropAre = document.getElementById('drop-area');
+
+  dropAre.style.display= "none";
+  progressBar.style.display = 'block';
+  progressBar.value = 0;
+  progressBar.style.backgroundColor = 'red';
+
+  progressBarUpDate(progressBar);
+}
+
+function progressBarUpDate(progressBar){
+ 
+  setTimeout(() => {
+    progressBar.value += 20;
+    console.log(progressBar.value);
+
+    if (progressBar.value >= 100) {
+      progressBar.style.display = 'none';
+    }else if (progressBar.value <= 100){
+      progressBarUpDate(progressBar)
+    }
+
+  }, 1000);
+}
+
+
+
